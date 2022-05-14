7679da8d4171e9601fff8080a57bca02584dc261
rasp <rasp@example.com>
2022-05-15 02:24:49 +0900

    jquery

--- eew/eew.js
+++ eew/eew.js
@@ -2,6 +2,13 @@ const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
 const { EEWGuilds, Rooms } = require('../db.js');
 
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const dom = new JSDOM(`<html><body><div id="aaa">AAA<div></body></html>`);
+const { document } = dom.window;
+const jquery = require('jquery');
+const $ = jquery(dom.window);
+
 var client;
 var timerId = null;
 var eews = {};
@@ -74,53 +81,68 @@ const getKMoni = async function() {
     ary[ary.length] = dblDigitStr(current.getSeconds());
     console.log(ary.join(''));
     try {
-        const res = await fetch(`http://www.kmoni.bosai.go.jp/webservice/hypo/eew/${ary.join('')}.json`);
-        const json = await res.json();
-        if (json.report_num != '') {
-            const report_id = json.report_id;
-            const report_num = json.report_num;
-            const isFinal = json.is_final;
-            var f = false;
-            for (id in eews) {
-                if (id == report_id && eews[id]["num"] == report_num) {
-                    f = true;
-                    break;
+        // const res = await fetch(`http://www.kmoni.bosai.go.jp/webservice/hypo/eew/${ary.join('')}.json`);
+        // const json = await res.json();
+        $.ajax({
+			type: "GET",
+			url: `http://www.kmoni.bosai.go.jp/webservice/hypo/eew/${ary.join('')}.json`,
+			dataType: "jsonp",
+			data: {},
+			async: true,
+			timeout: 0,
+			cache: true,
+			jsonp: "jsoncallback",
+			success: async function(json,status){
+                if (json.report_num != '') {
+                    const report_id = json.report_id;
+                    const report_num = json.report_num;
+                    const isFinal = json.is_final;
+                    var f = false;
+                    for (id in eews) {
+                        if (id == report_id && eews[id]["num"] == report_num) {
+                            f = true;
+                            break;
+                        }
+                    }
+                    if (!f) {
+                        if (!(report_id in textChannelMsg)) {
+                            textChannelMsg[report_id] = {};
+                        }
+                        eews[report_id] = {};
+                        eews[report_id]["num"] = report_num;
+                        eews[report_id]["origin_time"] = json.origin_time.substring(4,6) + '月'
+                                                     + json.origin_time.substring(6,8) + '日'
+                                                     + json.origin_time.substring(8,10) + '時'
+                                                     + json.origin_time.substring(10,12) + '分'
+                                                     + json.origin_time.substring(12,14) + '秒';
+                        eews[report_id]["region_name"] = json.region_name;
+                        eews[report_id]["magunitude"] = json.magunitude;
+                        eews[report_id]["calcintensity"] = json.calcintensity;
+                        eews[report_id]["embed"] = new MessageEmbed()
+                                .setColor('#c01717')
+                                .setTitle('緊急地震速報')
+                                .setURL('http://www.kmoni.bosai.go.jp/')
+                                .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
+                                .setDescription(`緊急地震速報　第${eews[report_id]['num']}報` + (isFinal ? " (最終報)" : ""))
+                                .addFields(
+                                    { name: '地震情報', value: `発生時刻: ${eews[report_id]['origin_time']}頃\n
+                                                              震源: ${eews[report_id]['region_name']}\n
+                                                              マグニチュード: ${eews[report_id]['magunitude']}\n
+                                                              最大震度: ${eews[report_id]['calcintensity']}` }
+                                )
+                                .setTimestamp();
+                        await sendMsg(report_id);
+                        return;
+                    }
+                } else {
+                    eews = {};
+                    textChannelMsg = {};
                 }
-            }
-            if (!f) {
-                if (!(report_id in textChannelMsg)) {
-                    textChannelMsg[report_id] = {};
-                }
-                eews[report_id] = {};
-                eews[report_id]["num"] = report_num;
-                eews[report_id]["origin_time"] = json.origin_time.substring(4,6) + '月'
-                                             + json.origin_time.substring(6,8) + '日'
-                                             + json.origin_time.substring(8,10) + '時'
-                                             + json.origin_time.substring(10,12) + '分'
-                                             + json.origin_time.substring(12,14) + '秒';
-                eews[report_id]["region_name"] = json.region_name;
-                eews[report_id]["magunitude"] = json.magunitude;
-                eews[report_id]["calcintensity"] = json.calcintensity;
-                eews[report_id]["embed"] = new MessageEmbed()
-                        .setColor('#c01717')
-                        .setTitle('緊急地震速報')
-                        .setURL('http://www.kmoni.bosai.go.jp/')
-                        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL() })
-                        .setDescription(`緊急地震速報　第${eews[report_id]['num']}報` + (isFinal ? " (最終報)" : ""))
-                        .addFields(
-                            { name: '地震情報', value: `発生時刻: ${eews[report_id]['origin_time']}頃\n
-                                                      震源: ${eews[report_id]['region_name']}\n
-                                                      マグニチュード: ${eews[report_id]['magunitude']}\n
-                                                      最大震度: ${eews[report_id]['calcintensity']}` }
-                        )
-                        .setTimestamp();
-                await sendMsg(report_id);
-                return;
-            }
-        } else {
-            eews = {};
-            textChannelMsg = {};
-        }
+			},
+			error: function(xhr,status,err){
+                
+			}
+		});
     } catch (err) {
         console.log(err);
     }
